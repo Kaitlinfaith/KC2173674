a71513b5587355a22b3cb0157441af77cf4bf6a6
Kaitlinfaith <kaitlinfaith@hotmail.com>
2014-06-25 19:32:22 -0700

    Signed-off-by: Kaitlinfaith <kaitlinfaith@hotmail.com>

---
+++ Homework/Hello_World_V1/main.cpp
@@ -0,0 +1,26 @@
+/* 
+ * File:   main.cpp
+ * Author: Kaitlin Conners
+ * Created on June 25, 2014, 7:18 PM
+ * Purpose: First Program Example
+ */
+
+
+#include <iostream>
+using namespace std;
+
+//User Defined Libraries
+
+//Global Constant
+
+//Function Prototypes
+
+//Execution Begins Here!
+int main(int argc, char** argv) {
+        //Output a string
+    cout<<"Hello World"<< endl;
+    //Exit stage right!
+    
+    return 0;
+}
+
